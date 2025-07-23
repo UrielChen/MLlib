3490cf9dc32372db87f60cbc313c4b14e7083f2c
PhilXu <philxu27@gmail.com>
2025-07-23 16:48:27 +0100

    add different dl model

--- src/modeling/network/__init__.py
+++ src/modeling/network/__init__.py
@@ -1,7 +1,7 @@
-from .mlp import mlp_model
 from .alexnet import alexnet_1d
-from .renet18_1d import resnet18_1d
-from .transformer.transformer import transformer
-from .vgg16_1d import vgg16_bn_1d
+from .mlp import mlp_model
 from .phil_nn_1 import phil_nn_1
-from .phil_nn_2 import phil_nn_2, phil_rp_shallow_network_1
+from .phil_nn_2 import phil_nn_2, phil_rp_shallow_network_1, phil_rp_model_a_shallow_wide, \
+    phil_rp_model_b_moderate_depth, phil_rp_model_c_deep_narrow, phil_rp_model_d_encoder_decoder, \
+    phil_rp_model_e_wide_then_sparse
+from .renet18_1d import resnet18_1d
--- src/modeling/network/phil_nn_2.py
+++ src/modeling/network/phil_nn_2.py
@@ -52,3 +52,88 @@ def phil_rp_shallow_network_1(cfg=None, **kwargs):
     hidden_dim = cfg.MODEL.HIDDEN_DIM if cfg else kwargs.get("HIDDEN_DIM")
     model = DeepResNetMLP(input_dim=input_dim, num_blocks=num_blocks, hidden_dim=hidden_dim, dropout=dropout)
     return model.to(device)
+
+@NETWORK_REGISTRY.register()
+def phil_rp_model_a_shallow_wide(cfg=None, **kwargs):
+    input_dim = cfg.MODEL.INPUT_DIM if cfg else kwargs.get("INPUT_DIM")
+    return DeepResNetMLP(input_dim=input_dim, hidden_dim=28, num_blocks=1, dropout=0.2).to(device)
+
+@NETWORK_REGISTRY.register()
+def phil_rp_model_b_moderate_depth(cfg=None, **kwargs):
+    input_dim = cfg.MODEL.INPUT_DIM if cfg else kwargs.get("INPUT_DIM")
+    return DeepResNetMLP(input_dim=input_dim, hidden_dim=16, num_blocks=3, dropout=0.2).to(device)
+
+@NETWORK_REGISTRY.register()
+def phil_rp_model_c_deep_narrow(cfg=None, **kwargs):
+    input_dim = cfg.MODEL.INPUT_DIM if cfg else kwargs.get("INPUT_DIM")
+    return DeepResNetMLP(input_dim=input_dim, hidden_dim=8, num_blocks=6, dropout=0.2).to(device)
+
+@NETWORK_REGISTRY.register()
+def phil_rp_model_d_encoder_decoder(cfg=None, **kwargs):
+    class Model(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.net = nn.Sequential(
+                nn.Linear(856, 24),
+                nn.ReLU(),
+                nn.Dropout(0.2),
+                nn.Linear(24, 6),
+                nn.ReLU(),
+                nn.Dropout(0.2),
+                nn.Linear(6, 24),
+                nn.ReLU(),
+                nn.Dropout(0.2),
+                nn.Linear(24, 1)
+            )
+        def forward(self, x):
+            return self.net(x)
+    return Model().to(device)
+
+@NETWORK_REGISTRY.register()
+def phil_rp_model_e_wide_then_sparse(cfg=None, **kwargs):
+    class TmpResidualBlock(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.linear1 = nn.Linear(20, 5)
+            self.linear2 = nn.Linear(5, 20)
+            self.bn1 = nn.BatchNorm1d(5)
+            self.bn2 = nn.BatchNorm1d(20)
+            self.relu = nn.ReLU()
+            self.dropout = nn.Dropout(0.2)
+
+        def forward(self, x):
+            identity = x
+            out = self.linear1(x)
+            out = self.bn1(out)
+            out = self.relu(out)
+            out = self.dropout(out)
+            out = self.linear2(out)
+            out = self.bn2(out)
+            out = self.dropout(out)
+            return self.relu(out + identity)
+
+    class Model(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.input = nn.Sequential(
+                nn.Linear(856, 20),
+                nn.BatchNorm1d(20),
+                nn.ReLU(),
+                nn.Dropout(0.2)
+            )
+            self.blocks = nn.Sequential(
+                TmpResidualBlock(),
+                TmpResidualBlock()
+            )
+            self.output = nn.Linear(20, 1)
+
+        def forward(self, x):
+            x = self.input(x)
+            x = self.blocks(x)
+            x = self.output(x)
+            return x
+
+    return Model().to(device)
+
+
+
